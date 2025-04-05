00672b5c4856e83744d4df103326fcbcd1756f9c
chanheui0102@gmail.com <chanheui0102@gmail.com>
2025-04-05 19:24:44 +0900

    [feat] 코멘트 pw 인증 수정

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,7 +1,7 @@
 // src/routes/comment.routes.ts
 import { Router } from 'express';
 import { CommentController } from '../controllers/comment.controller';
-import { nonUserOwnerMiddleware } from '../middlewares/non-user-owner.middleware';
+// import { nonUserOwnerMiddleware } from '../middlewares/non-user-owner.middleware';
 // import { ownerMiddleware } from '../middlewares/owner.middleware';
 const router = Router();
 const ctrl = new CommentController();
@@ -16,13 +16,9 @@ router.post('/password/validation', ctrl.validatePassword.bind(ctrl));
 router.post('/', ctrl.createNonUsersComment.bind(ctrl));
 
 // PATCH /comment (비회원 수정 시 nonUserOwnerMiddleware 검사)
-router.patch('/', nonUserOwnerMiddleware, ctrl.updateNonUserComment.bind(ctrl));
+router.patch('/', ctrl.updateNonUserComment.bind(ctrl));
 
 // DELETE /comment (비회원 삭제 시 nonUserOwnerMiddleware)
-router.delete(
-    '/',
-    nonUserOwnerMiddleware,
-    ctrl.deleteNonUserComment.bind(ctrl)
-);
+router.delete('/', ctrl.deleteNonUserComment.bind(ctrl));
 
 export default router;
